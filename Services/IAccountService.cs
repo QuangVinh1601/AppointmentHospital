bb5d73dbb8bb5e7b82f346a83f9caf6e2217639a
thedat123 <vothedatdavid@gmail.com>
2024-12-01 16:07:39 +0700

    Remove tracked files ignored by .gitignore

--- Services/IAccountService.cs
+++
@@ -1,9 +0,0 @@
-﻿using static AppointmentHospital.DTOs.Account.AccountRequest;
-namespace AppointmentHospital.Services
-{
-    public interface IAccountService
-    {
-        Task<bool> LoginAsync(LoginUserRequest request);
-        Task<bool> RegisterAsync(RegisterUserRequest request);
-    }
-}
